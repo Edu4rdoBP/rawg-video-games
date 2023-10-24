7b1dbfe3afcc73aab6432369b35feb1ce57c876f
Eduardo Carlos <eduardo@bitpreco.com>
2023-10-23 23:27:08 -0300

    🚧 [Progress]: card main

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Index() {
-  return <div></div>;
+  return <div style={{ backgroundColor: 'white', width: '1000px', height: '400px', borderRadius: 15, boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px' }}></div>;
 }
