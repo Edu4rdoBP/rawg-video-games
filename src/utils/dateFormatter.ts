6e2913b7f41a6784389d402f540f33ffa14580c5
Eduardo Carlos <eduardo@bitpreco.com>
2023-10-26 18:47:36 -0300

    ✨[Feature]: add date formatter

---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,13 @@
+export class DateFormatter {
+  static getFullDate() {
+    const today = new Date();
+
+    const year = today.getFullYear().toString().padStart(4, '0');
+    const month = (today.getMonth() + 1).toString().padStart(2, '0');
+    const day = today.getDate().toString().padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}`;
+
+    return formattedDate;
+  }
+}
