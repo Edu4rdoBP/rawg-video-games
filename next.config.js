950e8a2d54f0f174eb4a193f42d69c90f83588f3
Eduardo Carlos <eduardo@bitpreco.com>
2023-10-26 18:57:58 -0300

    📦 [Chore]: Add dependencies and configurations

--- next.config.js
+++ next.config.js
@@ -2,12 +2,10 @@
 const nextConfig = {
   compiler: {
     // Enables the styled-components SWC transform
-    styledComponents: true
-  }
+    styledComponents: true,
+  },
 };
 
-const withNextIntl = require('next-intl/plugin')(
-  './src/i18n.ts'
-);
+const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
 
 module.exports = withNextIntl(nextConfig);
