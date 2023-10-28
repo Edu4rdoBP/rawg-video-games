486bd8b1ab3d4ddffbb42f6bc1836290dbd7ed23
Eduardo Carlos <eduardo@bitpreco.com>
2023-10-27 21:53:46 -0300

    💄 [Feature]: Style pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,8 +1,10 @@
 'use client';
 import { IGameInfo, IGameInput } from '@/@types/Games';
-import React, { ReactNode, useCallback, useEffect, useState } from 'react';
+import React, { ReactNode, useCallback, useState } from 'react';
 import ReactPaginate from 'react-paginate';
-
+import * as S from './style';
+import { useTranslations } from 'next-intl';
+import { Tag } from '../Tag';
 interface IPaginationProps {
   games: IGameInfo[] | undefined;
   fetchGame: (input?: IGameInput) => void;
@@ -10,15 +12,13 @@ interface IPaginationProps {
   children: ReactNode;
 }
 
-export function Pagination({
-  games,
-  fetchGame,
-  loading,
-  children,
-}: IPaginationProps) {
+export function Pagination({ games, fetchGame, children }: IPaginationProps) {
   const [currentPage, setCurrentPage] = useState<number>(0);
-  const [itemsPerPage, setItemsPerPage] = useState<number>(8);
-  const count = games?.[0].count || 0;
+  const selectValues = [8, 16, 32, 64];
+  const [itemsPerPage, setItemsPerPage] = useState<number>(selectValues[0]);
+  const t = useTranslations();
+  const count = games?.[0]?.count || 0;
+
   const handlePageClick = useCallback(
     (data: { selected: number }) => {
       setCurrentPage(data.selected);
@@ -27,40 +27,54 @@ export function Pagination({
     [fetchGame, itemsPerPage],
   );
 
-  useEffect(() => {
-    if (!games && !loading) {
-      fetchGame({ page: 1, page_size: itemsPerPage });
-    }
-  }, [games, fetchGame, itemsPerPage, loading]);
+  const handlePageChangeSize = (value: number) => {
+    fetchGame({ page: currentPage + 1, page_size: value });
+    setItemsPerPage(value);
+  };
 
   const startIndex = currentPage * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const displayedNames = games?.slice(startIndex, endIndex);
   const pageCount = displayedNames ? Math.ceil(count / itemsPerPage) : 0;
 
-  const selectValues = [8, 16, 32, 64];
   return (
-    <div>
-      <select>
-        {selectValues.map((item) => {
-          return (
-            <option key={`select-item-per-page${item}`} value={item}></option>
-          );
-        })}
-      </select>
-      <div className='flex flex-wrap gap-[15px] justify-center'>{children}</div>
-      <ReactPaginate
-        pageCount={pageCount}
-        pageRangeDisplayed={5}
-        marginPagesDisplayed={1}
-        previousLabel={'Anterior'}
-        nextLabel={'Próximo'}
-        breakLabel={'...'}
-        onPageChange={handlePageClick}
-        containerClassName={'pagination'}
-        activeClassName={'active'}
-        initialPage={currentPage}
-      />
-    </div>
+    <>
+      <S.MenuPaginate>
+        <Tag>{t('Games.title')}</Tag>
+        <select
+          value={itemsPerPage}
+          onChange={(e) => {
+            handlePageChangeSize(Number(e.target.value));
+          }}
+        >
+          {selectValues.map((item) => {
+            return (
+              <option key={`select-item-per-page${item}`} value={item}>
+                {item} / {t('Pagination.perPage')}
+              </option>
+            );
+          })}
+        </select>
+      </S.MenuPaginate>
+      <S.Content>
+        <div className='flex flex-wrap gap-[20px] justify-center'>
+          {children}
+        </div>
+        <S.ReactPaginate className='font-big'>
+          <ReactPaginate
+            pageCount={pageCount}
+            pageRangeDisplayed={6}
+            marginPagesDisplayed={1}
+            previousLabel={'Anterior'}
+            nextLabel={'Próximo'}
+            breakLabel={'...'}
+            onPageChange={handlePageClick}
+            containerClassName={'pagination'}
+            activeClassName={'active'}
+            initialPage={currentPage}
+          />
+        </S.ReactPaginate>
+      </S.Content>
+    </>
   );
 }
