ff91075ea111c6e5d7ba406f7e81e142e7674849
Eduardo Carlos <eduardo@bitpreco.com>
2023-10-27 21:52:05 -0300

    ♻️ [Refactor]: Add bgColor to badge

--- src/components/Badge/style.ts
+++ src/components/Badge/style.ts
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import { IBadgePropsLayout } from './type';
 
 export const Badge = styled.span<IBadgePropsLayout>(
-  ({ theme, isSmall, color }) => `
+  ({ theme, isSmall, color, bgColor }) => `
   div {
     color: ${theme[color]};
-    background-color: ${isSmall ? theme.spaceGray : theme.badge};
+    background-color: ${bgColor ? theme[bgColor] : theme.spaceGray};
     border-radius: ${isSmall ? '8px' : '12px'};
     padding: ${isSmall ? '3px 10px' : '10px 20px'};
     font-family: var(--font-montserrat);
--- src/components/Badge/type.ts
+++ src/components/Badge/type.ts
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 export interface IBadgePropsLayout {
   isSmall?: boolean;
   color: string;
+  bgColor?: string;
 }
 
 export interface IBadgeProps extends IBadgePropsLayout {
