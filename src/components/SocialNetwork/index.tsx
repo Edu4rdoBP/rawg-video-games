f6c9980ec8a49a6d08bacfde060a1267bb32e74e
Eduardo Carlos <eduardo@bitpreco.com>
2023-10-28 23:50:28 -0300

    ➖ [Fix]: Remove Lazyload dependence

--- src/components/SocialNetwork/index.tsx
+++ src/components/SocialNetwork/index.tsx
@@ -1,6 +1,5 @@
 'use client';
 import * as S from './style';
-import LazyLoad from 'react-lazy-load';
 import Image from 'next/image';
 
 import Link from 'next/link';
@@ -13,15 +12,13 @@ export const SocialNetworks = () => {
     <S.SocialNetworks>
       {menuItems.map((item) => (
         <Link href={item.href} target='_blank' key={item.title}>
-          <LazyLoad offset={700}>
-            <Image
-              src={item.src}
-              alt={item.alt}
-              width={item.size}
-              height={item.size}
-              loading='lazy'
-            />
-          </LazyLoad>
+          <Image
+            src={item.src}
+            alt={item.alt}
+            width={item.size}
+            height={item.size}
+            loading='lazy'
+          />
         </Link>
       ))}
     </S.SocialNetworks>
