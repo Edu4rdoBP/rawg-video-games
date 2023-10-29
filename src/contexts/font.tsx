735d4318ce23659d6c2dc005d79e90339ee21233
Eduardo Carlos <eduardo@bitpreco.com>
2023-10-29 05:25:40 -0300

    ♻️ [Refactor]: Change name provider

--- src/contexts/font.tsx
+++ src/contexts/font.tsx
@@ -18,21 +18,21 @@ interface ActionsProviderProps {
   children: ReactNode;
 }
 
-const GlobalContext = createContext<ContextProps>({
+const FontSizeContext = createContext<ContextProps>({
   fontSize: 16,
   setFontSize: () => {},
 });
 
-export const GlobalContextProvider: React.FC<ActionsProviderProps> = ({
+export const FontSizeProvider: React.FC<ActionsProviderProps> = ({
   children,
 }: ActionsProviderProps) => {
   const [fontSize, setFontSize] = useState<number>(16);
 
   return (
-    <GlobalContext.Provider value={{ fontSize, setFontSize }}>
+    <FontSizeContext.Provider value={{ fontSize, setFontSize }}>
       {children}
-    </GlobalContext.Provider>
+    </FontSizeContext.Provider>
   );
 };
 
-export const useGlobalContext = () => useContext(GlobalContext);
+export const useFontSizeContext = () => useContext(FontSizeContext);
